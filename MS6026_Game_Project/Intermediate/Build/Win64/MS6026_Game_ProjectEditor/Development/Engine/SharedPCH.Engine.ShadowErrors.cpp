4fb04dbc7875d0c4f9f6633d0b51ca380668ac60
Thomas <u1909810@uel.ac.uk>
2021-12-06 16:43:58 +0000

    Additional changes (AI tweaks, retargeting experiments, etc.)

--- MS6026_Game_Project/Intermediate/Build/Win64/MS6026_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ MS6026_Game_Project/Intermediate/Build/Win64/MS6026_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30136
-#include "D:/MS6026_Game_Project/Intermediate/Build/Win64/MS6026_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30133
+#include "D:/Thomas Shadforth - 1909810/Github/MS6026_Proj/MS6026_Game_Project/Intermediate/Build/Win64/MS6026_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ MS6026_Game_Project/Intermediate/Build/Win64/MS6026_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30136
+#include "D:/MS6026_Game_Project/Intermediate/Build/Win64/MS6026_Game_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
